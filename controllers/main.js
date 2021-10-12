bb6a7db1c2873e36fbe7cb4c3f182301b0205c1c
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 13:18:13 +0530

    Add token authorization

--- controllers/main.js
+++ controllers/main.js
@@ -25,11 +25,18 @@ const dashboard = async (req, res) => {
   if (!(authHeader && authHeader.startsWith('Bearer '))) {
     throw new CustomAPIError('Unauthorized to access the route', 401);
   }
-  const luckyNumber = Math.floor(Math.random() * 100);
-  res.status(200).json({
-    msg: 'dashboard route',
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
+  const token = authHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const luckyNumber = Math.floor(Math.random() * 100);
+    res.status(200).json({
+      msg: `Hello, ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (err) {
+    throw new CustomAPIError('Not authorized to access this route', 401);
+  }
 };
 
 module.exports = {
