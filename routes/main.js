4e0a732d0c6c9d304ff23253f20dca8cb4cb2688
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 13:32:13 +0530

    Completed auth middleware

--- routes/main.js
+++ routes/main.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { login, dashboard } = require('../controllers/main');
+const authenticationMiddleware = require('../middleware/auth');
 const mainRouter = express.Router();
 
 mainRouter.route('/login').post(login);
-mainRouter.route('/dashboard').get(dashboard);
+mainRouter.route('/dashboard').get(authenticationMiddleware, dashboard);
 
 module.exports = mainRouter;
