f95fb7335869a1bec598a89a0468c1c4b8b9bb1b
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 12:08:11 +0530

    Fix typo

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,11 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  res.json({ msg: 'dashboard route' });
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: 'dashboard route',
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
