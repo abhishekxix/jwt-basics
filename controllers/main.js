ef366bda7982ab13bafdad14161b248b864001fa
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 12:20:59 +0530

    Add check for username and password

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,17 @@
+//check username, pwd in post(login) request
+// if exists: create new JWT and send it back to the front-end
+
+const CustomAPIError = require('../errors/custom-error');
+
+// Set up authentication
+
 const login = async (req, res) => {
-  res.json({ msg: 'Login route' });
+  const { username, password } = req.body;
+  if (username && password) {
+    return res.json({ msg: 'Login route' });
+  }
+
+  throw new CustomAPIError('Please enter email and password.', 400);
 };
 
 const dashboard = async (req, res) => {
