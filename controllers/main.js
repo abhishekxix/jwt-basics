e7a420559db150dfa710745cc2f8d68284980e67
Abhishek Singh <abhisheksinghxix@gmail.com>
2021-10-12 13:11:43 +0530

    Add check for token existence

--- controllers/main.js
+++ controllers/main.js
@@ -1,20 +1,30 @@
 //check username, pwd in post(login) request
 // if exists: create new JWT and send it back to the front-end
-
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 // Set up authentication
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  if (username && password) {
-    return res.json({ msg: 'Login route' });
+  if (!(username && password)) {
+    throw new CustomAPIError('Please enter email and password.', 400);
   }
+  // only for demo
+  const id = new Date().getDate();
+  // demo end
 
-  throw new CustomAPIError('Please enter email and password.', 400);
+  const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+  res.status(200).json({ msg: 'logged in', token });
 };
 
 const dashboard = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  if (!(authHeader && authHeader.startsWith('Bearer '))) {
+    throw new CustomAPIError('Unauthorized to access the route', 401);
+  }
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: 'dashboard route',
